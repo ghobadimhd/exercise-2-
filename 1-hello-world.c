91fb5ae8bb8fee3702237fecab277afc4722ec85
Ghobadimhd <Ghobadi.mhd@outlook.com>
2014-10-31 17:36:01 +0330

    #1.c: hello world program

---
+++ 1-hello-world.c
@@ -0,0 +1,15 @@
+/*
+1-hello-world	2014-10-31 Oct:10
+Program name : hello world
+Author : Mohammad Ghobadi
+Email : ghobadi.mhd@gmail.com
+describtion : This is program for printing "Hello world on screen"
+
+*/
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("Hello world .\n");
+	return 0;
+}
