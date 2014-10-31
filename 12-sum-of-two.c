5da5258f3a063fc1e1533748196ce6fb492dcd50
Ghobadimhd <Ghobadi.mhd@outlook.com>
2014-10-31 20:20:52 +0330

    #12.c sum of two with function program

---
+++ 12-sum-of-two.c
@@ -0,0 +1,17 @@
+/*
+12-sum-of-two	2014-10-31 Oct:10
+Program name : sum of two 
+Author : Mohammad Ghobadi
+Email : ghobadi.mhd@gmail.com
+describtion : This is program for calculating sum of two number with function
+*/
+#include <stdio.h>
+int sum(int, int );
+int main(int argc, const char *argv[])
+{
+	int number1 , number2 ;
+	printf("Please enter two number seprated with space : ");
+	scanf("%d %d ",&number1,&number2);
+	printf("sum is %d \n",sum(number1 , number2 ));
+	return 0;
+}
