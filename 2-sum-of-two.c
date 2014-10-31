21f650e8d85d4cd0fc25532fbe0d6349b8b88635
Ghobadimhd <Ghobadi.mhd@outlook.com>
2014-10-31 18:05:46 +0330

    #2.c sum of two program

---
+++ 2-sum-of-two.c
@@ -0,0 +1,17 @@
+/*
+2-sum-of-two	2014-10-31 Oct:10
+Program name : sum of two
+Author : Mohammad Ghobadi
+Email : ghobadi.mhd@gmail.com
+describtion : This is program for sum two number
+*/
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	int number1 , number2 ;
+	printf("Please enter two number seprated by space\n");
+	scanf("%d %d",&number1 , &number2);
+	printf("Sum = %d \n",number1 + number2 );
+	return 0;
+}
