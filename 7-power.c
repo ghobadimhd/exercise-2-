baeffac99a3b74812e2ebef5384753f4ed9f1226
Ghobadimhd <Ghobadi.mhd@outlook.com>
2014-10-31 18:38:03 +0330

    #7.c : power program

---
+++ 7-power.c
@@ -0,0 +1,22 @@
+/*
+7-power	2014-10-31 Oct:10
+Program name : power
+Author : Mohammad Ghobadi
+Email : ghobadi.mhd@gmail.com
+describtion : This is program for calculating r power n
+*/
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	double number , power , result=1 ;
+	printf("Please enter R and N seprated by space : ");
+	scanf("%f %f",&number,&power);
+	for (int i = 0; i <= power; i++) 
+	{
+		result *= number;
+	}
+	result = power < 0 ? 1/result : result;	
+	printf("R ^ N = %f\n",result);
+	return 0;
+}
