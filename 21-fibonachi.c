4d74e62bde15bb925bc0b1e7dd131f6dd4247b22
Ghobadimhd <Ghobadi.mhd@outlook.com>
2014-11-01 18:51:29 +0330

    #21.c : change algoritm to done the job with recursive function

--- 21-fibonachi.c
+++ 21-fibonachi.c
@@ -6,19 +6,30 @@ Email : ghobadi.mhd@outlook.com
 describtion : This is program for printing fibonachi series on screen  .
 */
 #include <stdio.h>
-
+int getFibonachi(int n);
 int main(int argc, const char *argv[])
 {
-	int n , a1=0 , a2 = 1 , temp;
+	int n ;
 	printf("Please enter a number : ");
 	scanf("%d",&n);
-	for (int i = 0; i <n; i++) 
+	for (int i = 0; i <=n; i++) 
 	{
-		printf("%d , ",a2);
-		temp = a2 ;
-		a2 += a1 ;
-		a1=temp ;
+		printf("%d , ",getFibonachi(i) );
 	}
 	printf("\b\b \n");
 	return 0;
+}
+/*
+this is function for geting Nth fibonachi number 
+*/
+int getFibonachi(int n)
+{
+	if ( n == 1 || n == 0)
+	{
+		 return 1;	
+	}
+	else 
+	{
+		return getFibonachi(n-1) + getFibonachi(n-2);
+	}
 }
