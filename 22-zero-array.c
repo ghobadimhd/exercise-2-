31ef6acd893341f9234b4fa5b5059e247f8c557d
Ghobadimhd <Ghobadi.mhd@outlook.com>
2014-11-01 18:58:37 +0330

    #22.c : zero array program

---
+++ 22-zero-array.c
@@ -0,0 +1,18 @@
+/*
+22-zero-array	2014-11-01 18:11
+Program name : zero array
+Author : Mohammad Ghobadi
+Email : ghobadi.mhd@outlook.com
+describtion : This is program for declaring array and fill it with zero .
+*/
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+	int array[10] ;
+	for (int i = 0; i <10; i++) 
+	{
+		printf("%d\n",array[10]);
+		
+	}
+	return 0;
+}
