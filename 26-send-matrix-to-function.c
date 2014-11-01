06fd8a6dcf126f6e08c5f2e6ce0ad677c6a4e7b7
Ghobadimhd <Ghobadi.mhd@outlook.com>
2014-11-01 21:15:57 +0330

    #26.c : send matrix to function

---
+++ 26-send-matrix-to-function.c
@@ -0,0 +1,14 @@
+/*
+26-send-matrix-to-function	2014-11-01 21:11
+Program name : send matrix to function
+Author : Mohammad Ghobadi
+Email : ghobadi.mhd@outlook.com
+describtion : This is program for showing how to send matrix to function .
+*/
+void function(int matrix[][2],int rows);
+int main(int argc, const char *argv[])
+{
+	int matrix[2][2] , rows=2;
+	function(matrix,rows);
+	return 0;
+}
