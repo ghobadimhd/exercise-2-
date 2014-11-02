a7dbda9f36df880f39e5f5f516f8f5bb906d05af
Ghobadimhd <Ghobadi.mhd@outlook.com>
2014-11-02 11:15:41 +0330

    #16.c : initial count word 2 program

---
+++ 16-count-word2.c
@@ -0,0 +1,35 @@
+/*
+16-count-word2	2014-11-02 11:11
+Program name : count word 2
+Author : Mohammad Ghobadi
+Email : ghobadi.mhd@outlook.com
+describtion : This is program for counting word with three letter .
+*/
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+        char *string ;
+        int count= 0 , i=0 , letterCount=0;
+        printf("Enter a string  : ");
+        gets(string);
+        while(string[i] != '\0')
+        {   
+                while (!(string[i] > 64 && string[i] <91) && !(string[i] > 96 && string[i]<123) && string[i] != '\0') 
+                         i++;
+                if(string[i] != '\0')
+                {   
+                        letterCount=1;
+                        i++ ;
+                        while ((string[i] > 64 && string[i] <91) || (string[i] > 96 && string[i]<123) ) 
+			{
+				letterCount++;
+                                i++;
+                        }
+			if(letterCount == 3 )
+				count++;
+                }   
+        }   
+        printf("there is %d number word in string .\n",count);
+        return 0;
+}
+
